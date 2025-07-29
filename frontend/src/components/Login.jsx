f6dc97ab9790df89b9447d26b458208a713b88a8
1000grams <AHprivateEMAIL@protonmail.com>
2025-07-28 21:25:53 -0400

    Fix Cognito login redirect

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-const loginUrl = "https://auth.decodedmusic.com/login?client_id=5pb29tja8gkqm3jb43oimd5qjt&response_type=code&scope=openid+email+profile&redirect_uri=https://decodedmusic.com/dashboard";
+const loginUrl =
+  "https://auth.decodedmusic.com/login?client_id=5pb29tja8gkqm3jb43oimd5qjt&response_type=token&scope=openid+email+profile&redirect_uri=https://decodedmusic.com/dashboard";
 
 const Login = () => {
   const redirectToCognito = () => {
